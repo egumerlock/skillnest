ab585964fd55b5fe876e5e6a8050b9c6a3ae44e7
OUCHUNYU <ouchunyu@yahoo.com>
2016-06-11 10:44:06 -0700

    Init files

---
+++ App/Components/Helpers/AnimatedRatingStars.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Easing,
+  Image,
+} from 'react-native';
+
+class AnimatedRatingStars extends Component {
+  constructor() {
+    super();
+    this.state = {
+      anims: [1,2,3,4,5].map(() => new Animated.Value(320))
+    }
+  }
+
+  componentDidMount() {
+    this.animateStarter();
+  }
+
+  animateStarter() {
+    var timing = Animated.timing;
+    Animated.sequence([ // One after the other
+
+      Animated.stagger(200,
+        this.state.anims.map((anim) => timing(anim, {
+          toValue: 0,
+          easing: Easing.bounce, // Like a ball
+          duration: 1000,
+        })),
+      )
+    ]).start();
+  }
+
+  render() {
+    return (
+      <View style={styles.startContainer}>
+        {[1, 2, 3, 4, 5].map(
+          (text, ii) => (
+            <Animated.View
+              key={text}
+              style={[{
+                left: this.state.anims[ii]
+              }]}>
+              <Image source={require('./star.png')}></Image>
+            </Animated.View>
+          )
+        )}
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  circleContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    flexWrap: 'wrap',
+    padding: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  startContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+export default AnimatedRatingStars;
---
+++ App/Components/StartSlider.js
@@ -0,0 +1,78 @@
+import Swiper from 'react-native-swiper';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+let styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  teachertButton: {
+    marginTop: 100,
+  },
+  studentButton: {
+    marginTop: 10,
+  },
+  startText: {
+    marginTop: 320,
+  }
+})
+
+class StartSlider extends Component {
+  render() {
+    return (
+      <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello There</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Welcome to...</Text>
+        </View>
+        <View style={styles.slide3}>
+          <View style={styles.startText}>
+            <Text style={styles.text}>SkillNest</Text>
+          </View>
+          <View style={styles.teachertButton}>
+            <Icon.Button name="ios-arrow-dropright-outline" backgroundColor="#3b5998" >
+              I'm a teacher
+            </Icon.Button>
+          </View>
+          <View style={styles.studentButton}>
+            <Icon.Button name="ios-arrow-dropright-outline" backgroundColor="#3b5998" >
+              I'm a student
+            </Icon.Button>
+          </View>
+        </View>
+      </Swiper>
+    )
+  }
+}
+
+export default StartSlider
