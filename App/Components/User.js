85b45dd8163c2e814dc3499b61c8360265839280
QuickResolve <lauranmontoya@gmail.com>
2016-06-11 12:40:12 -0700

    create user component

---
+++ App/Components/User.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+let styles = StyleSheet.create({
+
+})
+
+class User extends Component {
+  render() {
+    return (
+
+    )
+  }
+
+}
+
+export default User;
