42a66be1dfa1d3b5a30859882c3cb8a3dcaf95de
QuickResolve <lauranmontoya@gmail.com>
2016-06-11 15:05:14 -0700

    add mock data for classes

--- App/Components/User.js
+++ App/Components/User.js
@@ -5,6 +5,7 @@ import {
   Text,
   View,
   Image,
+  TouchableOpacity,
   ScrollView
 } from 'react-native';
 
@@ -14,12 +15,12 @@ let styles = StyleSheet.create({
    backgroundColor: '#ffffff'
   },
   hero: {
-    flex: 3,
+    flex: 3.25,
     flexDirection: 'column',
     backgroundColor: '#53D1E5'
   },
   history: {
-    flex: 5,
+    flex: 4,
     backgroundColor: '#43C6C6'
   },
   titleWrapper: {
@@ -34,14 +35,77 @@ let styles = StyleSheet.create({
     color: '#658D9F',
     padding: 15
   },
-  profile_pic:{
+  profilePic:{
     width: 175,
     height: 175,
     alignSelf: 'center',
     backgroundColor: 'transparent'
+  },
+  buttonWrapper: {
+    flex: .75,
+    flexDirection: 'row',
+  },
+  leftButton: {
+    flex: 0.25,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5388e5',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'white'
+  },
+  rightButton: {
+    flex: 0.25,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#53e5b0',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'white'
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white'
   }
-
 })
+
+var mockedUser = [
+  {
+    name: 'Molly S.',
+    email: 'mollys@gmail.com'
+    classes: {
+      1: {
+        title: 'Ombre Watercolor Workshop',
+        teacher: 'Jack J.',
+        imgLink: 'http://ih1.redbubble.net/image.58824777.0188/flat,1000x1000,075,f.u2.jpg',
+        lastContacted: 'June 11th, 2016'
+      },
+      2: {
+        title: 'Brush Lettering',
+        teacher: 'Bettie P.',
+        imgLink: 'http://static1.squarespace.com/static/540fd39ae4b01475ae2448ee/t/5448713fe4b0d7fec27158cf/1414033731198/',
+        lastContacted: 'June 1st, 2016'
+      },
+      3: {
+        title: 'Print Making',
+        teacher: 'Tanya B.',
+        imgLink: 'https://upload.wikimedia.org/wikipedia/commons/7/7f/Hokusai-fuji7.png',
+        lastContacted: 'May 28th, 2016'
+      }
+      4: {
+        title: 'EL Wire Workshop',
+        teacher: 'Matt F.',
+        imgLink: 'http://overyonderlust.com/wp-content/uploads/2014/01/EL_Wire.jpg',
+        lastContacted: 'May 15th, 2016'
+      }
+    },
+    reviews: {
+
+    }
+  }
+]
 
 class User extends Component {
   render(){
@@ -49,12 +113,28 @@ class User extends Component {
       <View style={styles.container}>
         <View style={styles.hero}>
         <View style={styles.titleWrapper}>
-          <Text style={styles.title}>Mandy S.</Text>
+          <Text style={styles.title}>mockedUser.name</Text>
         </View>
           <Image
-          style={styles.profile_pic}
+          style={styles.profilePic}
           source={{uri:'http://1.bp.blogspot.com/-I2aPA52ms38/VcqtGNT0-9I/AAAAAAAAGQ8/QTuHSROZl2c/s1600/abby-circular-profile.png'}}
           />
+        </View>
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity style={styles.leftButton}
+          underlayColor="transparent"
+          onPress={() => this._onClassesButton(this.props.id)}>
+          <Text style={styles.buttonText}>
+            Classes
+          </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.rightButton}
+          underlayColor="transparent"
+          onPress={() => this._onReviewsButton(this.props.id)}>
+          <Text style={styles.buttonText}>
+            Reviews
+          </Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.history}>
 
