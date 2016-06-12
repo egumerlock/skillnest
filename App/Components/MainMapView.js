f91d6874db863f5aa91bea78053317a7ae642c70
Eric Gumerlock <eric.gumerlock@gmail.com>
2016-06-11 20:15:59 -0700

    Update map page and add seed data

--- App/Components/MainMapView.js
+++ App/Components/MainMapView.js
@@ -1,6 +1,7 @@
 import Icon from 'react-native-vector-icons/Ionicons';
 import MapView from 'react-native-maps';
 import Spinner from 'react-native-spinkit';
+import AnimatedRatingStars from './Helpers/AnimatedRatingStars'
 import React, { Component } from 'react';
 var CustomCallout = require('./CustomCallout.js')
 import {
@@ -34,7 +35,7 @@ class MainMapView extends Component {
       selectedTab: 'GoogleMap',
       type: 'Bounce',
       size: 80,
-      color: "#32cd32",
+      color: "#4598ff",
       isVisible: true,
       loadAll: false,
       region: {
@@ -46,13 +47,82 @@ class MainMapView extends Component {
       markers: [
         {
           coordinate: {
+            latitude: 37.7824,
+            longitude: -122.4995,
+          },
+          name: "Jim Hadley",
+          field: "Surf Instructor",
+          capacity: "8/8",
+          pricing: "$$",
+          avatar: 'https://www.shoptab.net/blog/wp-content/uploads/2014/07/profile-circle.png'
+        },
+        {
+          coordinate: {
             latitude: 37.8324,
             longitude: -122.4795,
           },
-          name: "Tina Fey"
+          name: "Megan Dudley",
+          field: "Surf Instructor",
+          capacity: "10/17",
+          pricing: "$",
+          avatar: 'http://1.bp.blogspot.com/-I2aPA52ms38/VcqtGNT0-9I/AAAAAAAAGQ8/QTuHSROZl2c/s1600/abby-circular-profile.png'
         },
-      ]
-
+        {
+          coordinate: {
+            latitude: 37.7324,
+            longitude: -122.4795,
+          },
+          name: "Jeanne Renault",
+          field: "Surf Instructor",
+          capacity: "5/6",
+          pricing: "$$$",
+          avatar: 'http://i.imgur.com/gKOpj8v.png'
+        },
+        {
+          coordinate: {
+            latitude: 37.7924,
+            longitude: -122.3995,
+          },
+          name: "Jacques LeMans",
+          field: "Surf Instructor",
+          capacity: "16/24",
+          pricing: "$$",
+          avatar: 'http://static1.squarespace.com/static/526839d5e4b0a6ea6c312276/526ef8b1e4b0aa6f78f3f614/5271642ce4b03e61739879b6/1383162937731/tim+in+India+profile_circle.png'
+        },
+        {
+          coordinate: {
+            latitude: 37.7424,
+            longitude: -122.3995,
+          },
+          name: "Thomas Linea",
+          field: "Surf Instructor",
+          capacity: "2/4",
+          pricing: "$$$",
+          avatar: 'http://s3-us-west-2.amazonaws.com/s.cdpn.io/6083/profile/profile-512_1.jpg'
+        },
+        {
+          coordinate: {
+            latitude: 37.7484,
+            longitude: -122.4395,
+          },
+          name: "Sochie Lee",
+          field: "Surf Instructor",
+          capacity: "2/4",
+          pricing: "$$$",
+          avatar: 'https://www.sochiie.com/wp-content/uploads/2014/04/facebook-teerasej-profile-ball-circle.png'
+        },
+        {
+          coordinate: {
+            latitude: 37.7724,
+            longitude: -122.4395,
+          },
+          name: "Doug Bodder",
+          field: "Surf Instructor",
+          capacity: "8/8",
+          pricing: "$$",
+          avatar: 'http://dynamicinfluence.com/wp-content/uploads/2014/06/Robert-Circle-Profile-Pic.png'
+        },
+      ],
     };
   }
 
@@ -62,11 +132,21 @@ class MainMapView extends Component {
   componentWillMount() {
     setTimeout(() => {
       this.setState({isVisible: false, loadAll: true});
-    }, 0);
+    }, 2500);
+  }
+  startTrigger() {
+      this.setState({
+        startIndicator: true
+      })
+      setTimeout(() => {
+      this.setState({startIndicator: false});
+      }, 5000);
+
   }
 
 
   render() {
+    var starHolder = this.state.startIndicator ? <AnimatedRatingStars /> : <View></View>;
     const { region, markers } = this.state;
     var markersList = this.state.markers.map((item, index) => {
     return (
@@ -79,16 +159,50 @@ class MainMapView extends Component {
         coordinate={markers[index].coordinate}
         calloutAnchor={{ x: 0.1, y: 0.1 }}
         calloutOffset={{ x: -7, y: 29 }}
+        onPress={this.startTrigger.bind(this)}
       >
         <Image source={require('./Helpers/mapicon.png')} onLoadEnd={() => {if (!this.state.iconLoaded) this.setState({iconLoaded: true});}}/>
         <MapView.Callout tooltip>
           <TouchableOpacity>
             <CustomCallout style={styles.calloutOpacity}>
-              <View style={styles.wrapper}>
-                <Text style={styles.calloutHeader}>{markers[index].name}</Text>
+            <View style={styles.container}>
+
+                <View style={styles.hero}>
+                <View style={styles.titleWrapper}>
+                  <Text style={styles.title}>{markers[index].name} </Text>
+                </View>
+                  <Image
+                  style={styles.profilePic}
+                  source={{uri: markers[index].avatar}}
+                  />
+                  <View style={styles.footerWrapper}>
+                    <Text style={styles.footerHeader}>{markers[index].field} </Text>
+                    {starHolder}
+                    <Text style={styles.footerText}>Availability: {markers[index].capacity} </Text>
+                    <Text style={styles.footerTextBottom}>Pricing: {markers[index].pricing} </Text>
+                  </View>
+                </View>
+            
+              <View style={styles.buttonWrapper}>
+                <TouchableOpacity style={styles.leftButton}
+                underlayColor="transparent"
+                onPress={() => this._onClassesButton(this.props.id)}>
+                <Text style={styles.buttonText}>
+                  Classes
+                </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.rightButton}
+                underlayColor="transparent"
+                onPress={() => this._onProfileButton(this.props.id)}>
+                <Text style={styles.buttonText}>
+                  Profile
+                </Text>
+                </TouchableOpacity>
               </View>
-              <Text style={styles.calloutText}> </Text>
-              <Image style={styles.calloutImage} source={require('./Helpers/star.png')}/>
+              <View style={styles.history}>
+
+              </View>
+            </View>
             </CustomCallout>
           </TouchableOpacity>
         </MapView.Callout>
@@ -113,7 +227,7 @@ class MainMapView extends Component {
     return (
       <TabBarIOS
         unselectedTintColor="white"
-        tintColor="green"
+        tintColor='#53D1E5'
         barTintColor="white">
         <Icon.TabBarItemIOS
           iconName={"ios-text-outline"}
@@ -204,7 +318,7 @@ const styles = StyleSheet.create({
   },
   calloutOpacity: {
     borderRadius: 8,
-    opacity: .8
+    opacity: .90
   },
   icon: {
     height: 25,
@@ -236,6 +350,101 @@ const styles = StyleSheet.create({
     flex: 1,
     height: 32,
     width: 22
+  },
+  container: {
+   flex: 1,
+   backgroundColor: '#ffffff'
+  },
+  hero: {
+    flex: 3.25,
+    flexDirection: 'column',
+    backgroundColor: '#53D1E5'
+  },
+  history: {
+    flex: 4,
+    backgroundColor: '#43C6C6'
+  },
+  titleWrapper: {
+    justifyContent: 'center',
+    backgroundColor: 'white',
+
+    flexDirection: 'row'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#658D9F',
+    padding: 15
+  },
+  profilePic:{
+    width: 125,
+    height: 125,
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+    marginVertical: 40,
+  },
+  buttonWrapper: {
+    flex: .75,
+    flexDirection: 'row',
+  },
+  leftButton: {
+    flex: 0.25,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4598ff',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'white'
+  },
+  rightButton: {
+    flex: 0.25,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#53e5b0',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'white'
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white'
+  },
+  field: {
+    color: 'white',
+    margin: 5
+  },
+  footerWrapper: {
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  footerHeader: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: '#658D9F',
+    marginTop: 10,
+    marginBottom: 6,
+  },
+  footerText: {
+    textAlign: 'center',
+    fontSize: 12,
+    color: '#658D9F',
+    justifyContent: 'center',
+    marginBottom: 6,
+    marginTop: 10,
+  },
+  footerTextBottom: {
+    textAlign: 'center',
+    fontSize: 12,
+    color: '#658D9F',
+    justifyContent: 'center',
+    marginBottom: 6,
+  },
+  spinner: {
+    marginLeft: 152,
+    marginBottom: 20
   }
 
 });
