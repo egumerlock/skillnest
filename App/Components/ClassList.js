2c2e7ab75bff00fb4cdbf21dfce9c675600bd155
QuickResolve <lauranmontoya@gmail.com>
2016-06-11 19:50:00 -0700

    set up list population for seed data

--- App/Components/ClassList.js
+++ App/Components/ClassList.js
@@ -14,18 +14,47 @@ let styles = StyleSheet.create({
   container: {
    flex: 1,
    paddingTop: 20,
-   backgroundColor: '#ffffff'
+   backgroundColor: '#43C6C6'
   },
 })
 
 class ClassList extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2,
+      }),
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(mockTeacher[0].courses),
+      loaded: true
+    })
+  }
+
+  renderCourses(course) {
+    return (
+      <CourseCell course={course} />
+    )
+  }
+
   render() {
     return(
-      <View>
-
+      <View style={styles.container}>
+        <BrowseClassesView />
       </View>
     )
   }
 }
+
+
 
 export default ClassList;
